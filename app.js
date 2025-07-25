05e9f122ddb03ccb00541c86d928adbc37de3105
aditya_khadase <adityakhadase345@gmail.com>
2025-07-25 16:54:38 +0530

    add new feature

--- app.js
+++ app.js
@@ -1,23 +1,46 @@
-const input =document.getElementById("input-box");
-const ul =document.querySelector("ul");
-function addtask(){
+const input = document.getElementById("input-box");
+const ul = document.querySelector("ul");
+
+function addtask() {
     if (input.value === '') {
         alert("You must write something!");
-        return; // Stop the function here
+        return;
     }
     let li = document.createElement("li");
     li.innerText = input.value;
     ul.appendChild(li);
     li.classList.add("uncheck");
-    var span =document.createElement("img")
-    span.src ="./close.png"
-    span.type ="button";
-    li.appendChild(span)
-    input.value ="";
-    span.onclick=function(){
-li.remove();
-};
-   
-};
 
+    let span = document.createElement("img");
+    span.src = "./close.png";
+    span.type = "button";
+    li.appendChild(span);
 
+    span.onclick = function () {
+        li.remove();
+        savedata();
+    };
+
+    input.value = "";
+    savedata();
+}
+
+function savedata() {
+    localStorage.setItem("newtododata", ul.innerHTML);
+}
+
+function attachDeleteHandlers() {
+    document.querySelectorAll("ul li img").forEach(span => {
+        span.onclick = function () {
+            span.parentElement.remove();
+            savedata();
+        };
+    });
+}
+
+function showdata() {
+    ul.innerHTML = localStorage.getItem("newtododata");
+    attachDeleteHandlers(); // Important: reattach the delete logic!
+}
+
+showdata();
